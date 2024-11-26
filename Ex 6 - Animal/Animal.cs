675049d8e4981a51d15c411ac32d3acbf1f1f7a4
IsadoraDias19 <isadora.d.souza@aluno.senai.br>
2024-11-26 09:24:35 -0300

    Adicionar arquivos de projeto.

---
+++ Ex 6 - Animal/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_6___Animal
+{
+    public abstract class Animal
+    {
+        public string Especie { get; set; }
+        public abstract void EmitirSom();
+    }
+
+    public class Gato : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("O GATO FAZ miau, miau");
+        }
+    }
+
+    public class Porco : Animal
+    {
+       
+        public override void EmitirSom()
+        {
+            Console.WriteLine("O PORCO FAZ oinc, oinc");
+        }
+    }
+
+
+    public class Capivara : Animal
+    {
+    
+        public override void EmitirSom()
+        {
+            Console.WriteLine("A CAPIVARA FAZ ahn, ahn");
+        }
+    }
+}
---
+++ Ex 6 - Animal/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ex_6___Animal;
+
+Gato gato1 = new Gato
+{
+   Especie = "Maine Coon"
+
+};
+
+Porco porco1 = new Porco
+{
+    Especie = "Javali"
+
+};
+
+Capivara capi1 = new Capivara
+{
+    Especie = "Hydrochoerus hydrochaeris"
+
+};
+
+gato1.EmitirSom();
+porco1.EmitirSom();
+capi1.EmitirSom();
+
+Console.WriteLine($"A especie do gato é {gato1.Especie}, do porco é {porco1.Especie}, e da capivara é {capi1.Especie} ");
+Console.ReadKey();
